f8d971aa00eddd1d75c44db252e5745f0bcc722a
Lucas Sampaio <lukaz.sampaio@gmail.com>
2021-06-12 15:27:24 -0300

    first commit

---
+++ app/src/main/java/lab/venturus/lancadados/RegisterUserFragment.kt
@@ -0,0 +1,40 @@
+package lab.venturus.lancadados
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import lab.venturus.lancadados.databinding.FragmentRegisterUserBinding
+
+class RegisterUserFragment : Fragment() {
+    private var binding: FragmentRegisterUserBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterUserBinding.inflate(inflater, container, false)
+
+        val playerName = binding?.inputPlayerName
+        val getPlayerName = binding?.btnGetPlayerName
+
+        getPlayerName?.setOnClickListener {
+            val player = playerName?.text.toString()
+            findNavController().navigate(
+                R.id.action_navigation_register_to_navigation_dashboard
+//                ,                bundleOf("player" to player)
+                )
+        }
+        return binding?.root
+    }
+}
---
+++ app/src/main/java/lab/venturus/lancadados/ThrowDicesFragment.kt
@@ -0,0 +1,62 @@
+package lab.venturus.lancadados
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import lab.venturus.lancadados.databinding.FragmentThrowDicesBinding
+
+class ThrowDicesFragment : Fragment() {
+    var binding: FragmentThrowDicesBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentThrowDicesBinding.inflate(inflater, container, false)
+
+        val dice01 = binding?.imgDice01
+        val dice02 = binding?.imgDice02
+        val btnPlay = binding?.btnJogar
+        val welcome = binding?.textViewWelcome
+        val shareButton = binding?.share
+
+        val player = "Player Name"
+        welcome?.text = arguments?.getString("player")
+//        val message = getString(R.string.welcome, player)
+
+
+        shareButton?.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, "You are a lucky!")
+//            intent.setPackage("com.whatsapp")
+            intent.type = "text/plain"
+
+//            activity?.packageManager?.run {
+//                if (intent.resolveActivity(this) != null) {
+//                    startActivity(intent)
+//                } else {
+//                    Toast.makeText(context, "You don't have WhatsApp app", Toast.LENGTH_LONG).show()
+//                }
+//            }
+        }
+
+        btnPlay?.setOnClickListener {
+            dice01?.setImageResource(randomDices())
+            dice02?.setImageResource(randomDices())
+        }
+
+        return binding?.root
+    }
+
+    private fun randomDices() = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6,
+    ).random()
+}
